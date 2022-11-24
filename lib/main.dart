93729536ecb48e6a12f06bee290be705280942cc
Thomas Riedl <thomas.riedl@htl-wels.at>
2022-11-24 15:58:11 +0100

    fixed eRrOr

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:schwammerlapp/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +6,6 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: 'SchwammerlAPP',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
@@ -23,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WidgetTree(),
+      home: const WidgetTree(),
     );
   }
 }
